fdd2166b1fb157e07f84f0716f6007d3e1d75de3
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-03-03 17:59:23 +0100

    feat: change useFetch

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,30 +1,36 @@
 import { useEffect, useState } from 'react';
 
 const useFetch = (url) => {
-   const [data, setData] = useState([]);
-   const [loading, setLoading] = useState(false);
+   const [data, setData] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
    const [error, setError] = useState(null);
 
    useEffect(() => {
       const getData = async () => {
-         setLoading(true);
          try {
-            const resp = await fetch(url);
-            const data = await resp.json();
-            localStorage.setItem('data', JSON.stringify(data));
-            setData(data);
+            const dataStorage = localStorage.getItem('data');
+            if (dataStorage) {
+               setData(JSON.parse(localStorage.getItem('data')));
+               setIsLoading(false);
+            } else {
+               const resp = await fetch(url);
+               const data = await resp.json();
+               localStorage.setItem('data', JSON.stringify(data));
+               setData(data);
+               setIsLoading(false);
+            }
          } catch (error) {
             console.log(error);
             setError(error);
          } finally {
-            setLoading(false);
+            setIsLoading(false);
          }
       };
 
       getData();
    }, []);
 
-   return { data, loading, error };
+   return { data, isLoading, error };
 };
 
 export default useFetch;
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -5,9 +5,7 @@ import { useFetch } from '../hooks';
 const URL = '../../db.json';
 
 const Booking = () => {
-   const { loading, error } = useFetch(URL);
-   const [data, setData] = useState(JSON.parse(localStorage.getItem('data')));
-   // console.log('data:', data);
+   const { isLoading, error, data } = useFetch(URL);
 
    const [change, setChange] = useState();
 
@@ -20,6 +18,13 @@ const Booking = () => {
       console.log(e.target.value);
       setData([...data, { [e.target.name]: parseInt(e.target.quantity - 1) }]);
    };
+
+   if (isLoading) {
+      return <p>Loading...</p>;
+   }
+   if (error) {
+      return <p>ERROR</p>;
+   }
 
    return (
       <>
@@ -36,24 +41,23 @@ const Booking = () => {
                      </tr>
                   </thead>
                   <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                     {data &&
-                        data?.map((d) => (
-                           <tr key={d.id}>
-                              <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
-                                 {d.name}
-                              </td>
-                              <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-                                 <button onClick={(e) => handleAdd(e)} value={d.quantity} name={d.name}>
-                                    +
-                                 </button>
-                              </td>
-                              <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-                                 <button onClick={(e) => handleSub(e)} value={d.quantity} name={d.name}>
-                                    -
-                                 </button>
-                              </td>
-                           </tr>
-                        ))}
+                     {data.map((d) => (
+                        <tr key={d.id}>
+                           <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 dark:text-white">
+                              {d.name}
+                           </td>
+                           <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
+                              <button onClick={(e) => handleAdd(e)} value={d.quantity} name={d.name}>
+                                 +
+                              </button>
+                           </td>
+                           <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
+                              <button onClick={(e) => handleSub(e)} value={d.quantity} name={d.name}>
+                                 -
+                              </button>
+                           </td>
+                        </tr>
+                     ))}
                   </tbody>
                </table>
             </div>
