a0eb3a8334a71bb1b06e0351c204536d2af25400
EnPere <pereruba@gmail.com>
2023-03-10 09:41:54 +0100

    update datepicker

--- src/pages/TestingP.jsx
+++ src/pages/TestingP.jsx
@@ -119,4 +119,26 @@ function TestingP() {
   );
 }
 
-export default TestingP;
+
+// TEST CHATGPT
+// export default TestingP;
+
+// function useForm(initialValues) {
+//   const [values, setValues] = useState(initialValues);
+
+//   const handleChange = (event) => {
+//     const { name, value } = event.target;
+//     setValues({ ...values, [name]: value });
+//   };
+
+//   const resetForm = () => {
+//     setValues(initialValues);
+//   };
+
+//   return {
+//     values,
+//     handleChange,
+//     resetForm,
+//   };
+// }
+
